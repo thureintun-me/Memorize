022828c37373f235b508c469d89e3df4365019b6
thurein <thureintun@acedatasystems.com>
2024-07-12 10:12:16 +0630

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by ThureinTun on 12/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
